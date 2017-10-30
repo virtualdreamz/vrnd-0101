90e0052c18f5c41284df6af73379667480de435f
virtualdreamz <virtualdreamz@hotmail.com>
2017-10-30 07:50:01 -0400

    ADD Unity Project

---
+++ vrnd-01-01-Carnival-UNITY/Assets/UdacityVR/Scripts/MenuStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour {
+
+    public void startGame(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
